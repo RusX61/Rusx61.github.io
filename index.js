1fb2e7d7daf1d65e3dc5a5c4ece7059ed1ec5761
Ruslan Merzlyakov <rusx.lan@gmail.com>
2022-07-13 15:07:12 +0300

    ok

---
+++ index.js
@@ -0,0 +1,26 @@
+
+submitButton.onclick = function() {
+    let weight = document.getElementById("userWeight").value;
+    let height = document.getElementById('userHeight').value;
+    let age = document.getElementById('userAge').value;
+    let gender = document.querySelector('input[name = "gender"]:checked').value;
+    let imt = weight/((height/100)**2);
+    
+    if (gender == 'male') {
+        result = (height - 100)*1.15;;
+    } else result = (height - 110)*1.15;
+
+    if(age < 30){
+        result=result-((result/100)*11);
+    }else if (age > 50){
+        result=result+((result/100)*6);
+    }
+
+    if(result>weight){
+        recomendation = 'Ваш вес меньше нормы,стоит набрать массу.';
+    } else recomendation = 'Ваш вес больше нормы,стоит сесть на диету и заняться спортом.'
+
+    alert("Индекс Массы вашего тела = " + imt + "\x0A" + 
+    'Ваш нормальный вес: '+ result + 'КГ' + '\x0A' + recomendation);
+}
+
