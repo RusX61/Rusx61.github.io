f4b9de9c6ad0e5c0a0475728d8265359cd85dde4
Ruslan Merzlyakov <rusx.lan@gmail.com>
2022-07-16 00:35:23 +0300

    redesign

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 submitButton.onclick = function() {
     let weight = document.getElementById("userWeight").value;
     let height = document.getElementById('userHeight').value;
@@ -24,3 +23,26 @@ submitButton.onclick = function() {
     'Ваш нормальный вес: '+ result + 'КГ' + '\x0A' + recomendation);
 }
 
+function openPage(pageName, elmnt, color) {
+  // Hide all elements with class="tabcontent" by default */
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+  }
+
+  // Remove the background color of all tablinks/buttons
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].style.backgroundColor = "";
+  }
+
+  // Show the specific tab content
+  document.getElementById(pageName).style.display = "block";
+
+  // Add the specific color to the button used to open the tab content
+  elmnt.style.backgroundColor = color;
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
